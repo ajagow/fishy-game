2e823108dec75ba52c5ce12d4e78014b37fa7be7
Anika Jagow <anikajagow@Anikas-MacBook-Pro.local>
2026-05-15 21:17:07 -0700

    fix copy

--- src/components/DesktopOnly/DesktopOnly.tsx
+++ src/components/DesktopOnly/DesktopOnly.tsx
@@ -9,7 +9,7 @@ export const DesktopOnly = ({ children }: React.PropsWithChildren) => {
     return (
       <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
         <Text variant="title">Sry! Fishy is not available on mobile</Text>
-        <Text variant="body">Please visit on a desktop device to play. Our devs are working on hard to support a mobile version!</Text>
+        <Text variant="body">Please visit on a desktop device to play. Our devs are working overtime to support a mobile version!</Text>
       </div>
     )
   }
